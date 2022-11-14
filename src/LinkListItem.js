d9db986a69c652ebc50a9e000ece074133f6c609
Lee-Roy King <leeroywking@gmail.com>
2022-11-14 11:37:49 -0800

    updated some dependancies

--- src/LinkListItem.js
+++ src/LinkListItem.js
@@ -3,12 +3,12 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 
-export default (props) => (
+const main = (props) => (
   <ListItemButton
-    component={props.component}
-    href={props.href}
-    onClick={props.onClick || null}
-    sx={props.sx}
+  component={props.component}
+  href={props.href}
+  onClick={props.onClick || null}
+  sx={props.sx}
   >
     <ListItemIcon>
       {props.icon}
@@ -19,3 +19,5 @@ export default (props) => (
     {props.children}
   </ListItemButton>
 );
+
+export default main
